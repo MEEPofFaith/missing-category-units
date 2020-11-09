402e4a7f7fcea9dd374d95672b01a090418572a4
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2020-11-09 14:11:31 -0500

    Update spear.js

--- scripts/units/spear.js
+++ scripts/units/spear.js
@@ -1,4 +1,4 @@
 const SpAirT3 = extendContent(UnitType, "spear", {});
 SpAirT3.constructor = () => extend(MinerUnit, {});
 SpAirT3.defaultController = () => new FlyingAI();
-//public static @EntityDef({Unitc.class, Builderc.class, Minerc.class, Trailc.class}) UnitType spear; doesn't work, missing "type"
+public static @EntityDef({Unitc.class, Builderc.class, Minerc.class, Trailc.class}) UnitType dart, arrow, spear, javelin;
