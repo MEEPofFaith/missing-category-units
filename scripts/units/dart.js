5c31b715e1a867abd437d67dd96bebd4cdf51368
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2020-11-09 11:02:42 -0500

    Update dart.js

--- scripts/units/dart.js
+++ scripts/units/dart.js
@@ -1,4 +1,4 @@
 const SpAirT1 = extendContent(UnitType, "dart", {});
 SpAirT1.constructor = () => extend(MinerUnit, {});
 SpAirT1.defaultController = () => new FlyingAI();
-SpAirT1.abilities.add(new MoveLightningAbility(10, 8, 0.1, 1.5, 4, Color.valueOf("bf92f9")));
+SpAirT1.abilities.add(new MoveLightningAbility(10, 8, 0.1, 1.5, 4, Color.valueOf("#bf92f9")));
