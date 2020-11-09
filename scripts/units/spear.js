252f446a2708299e3690cd0cc4c68746f2445b4c
MEEP of Faith <meepmatthew@gmail.com>
2020-11-09 13:06:14 -0800

    Fix Spear Ability

--- scripts/units/spear.js
+++ scripts/units/spear.js
@@ -2,4 +2,4 @@ const SpAirT3 = extendContent(UnitType, "spear", {});
 SpAirT3.constructor = () => extend(BuilderMinerUnit, {});
 SpAirT3.defaultController = () => new FlyingAI();
 SpAirT3.abilities.add(new MoveLightningAbility(14, 16, 0.15, 2, 5, Color.valueOf("#bf92f9")));
-SpAirT3.abilities.add(new StatusFieldAbility(Status.overdrive, 90, 120, 48));
+SpAirT3.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 90, 120, 48));
