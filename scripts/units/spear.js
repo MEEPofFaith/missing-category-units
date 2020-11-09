e57e67c5f8583cdf8d06cfcb554071d3de887218
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2020-11-09 11:59:56 -0500

    Update spear.js

--- scripts/units/spear.js
+++ scripts/units/spear.js
@@ -1,3 +1,4 @@
 const SpAirT3 = extendContent(UnitType, "spear", {});
 SpAirT3.constructor = () => extend(MinerUnit, {});
 SpAirT3.defaultController = () => new FlyingAI();
+public static @EntityDef({Unitc.class, Builderc.class, Minerc.class, Trailc.class}) UnitType spear;
