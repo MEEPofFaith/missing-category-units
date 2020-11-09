dfe1a8ce10303dc15bd727e33c66e3427fe0110c
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2020-11-09 18:09:51 -0500

    Update arrow.js

--- scripts/units/arrow.js
+++ scripts/units/arrow.js
@@ -1,4 +1,4 @@
 const SpAirT2 = extendContent(UnitType, "arrow", {});
-SpAirT2.constructor = () => extend(BuilderMinerUnit,, {});
+SpAirT2.constructor = () => extend(BuilderMinerUnit, {});
 SpAirT2.defaultController = () => new FlyingAI();
 SpAirT2.abilities.add(new MoveLightningAbility(11, 12, 0.15, 1.75, 4.5, Color.valueOf("#bf92f9")));
