3b98596825cf65b61f422c8bae06c9dae7887fcf
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2020-11-10 11:52:40 -0500

    Update rana.js

--- scripts/units/rana.js
+++ scripts/units/rana.js
@@ -2,4 +2,4 @@ const SNavT1 = extendContent(UnitType, "rana", {});
 SNavT1.constructor = () => extend(NavalUnit, {});
 SNavT1.defaultController = () => new GroundAI();
 
-Blocks.airFactory.plans.add(new UnitFactory.UnitPlan(SpAirT1, 60 * 25, ItemStack.with(Items.silicon, 45, Items.lead, 25, Items.titanium, 20)));
+Blocks.navalFactory.plans.add(new UnitFactory.UnitPlan(SpAirT1, 60 * 25, ItemStack.with(Items.silicon, 45, Items.lead, 25, Items.titanium, 20)));
