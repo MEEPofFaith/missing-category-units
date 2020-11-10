8310f4c0903247dc7ba2b74b4e6b66539ec848be
MEEP of Faith <meepmatthew@gmail.com>
2020-11-10 15:26:38 -0800

    fix

--- scripts/units/sNaval/rana.js
+++ scripts/units/sNaval/rana.js
@@ -5,4 +5,4 @@ SuNavT1.defaultController = () => new GroundAI();
 SuNavT1.abilities.add(new RepairFieldAbility(10, 60 * 5, 24));
 SuNavT1.abilities.add(new ShieldRegenFieldAbility(40, 50, 60 * 8, 24));
 
-Blocks.navalFactory.plans.add(new UnitFactory.UnitPlan(SNavT1, 60 * 25, ItemStack.with(Items.silicon, 45, Items.lead, 25, Items.titanium, 20)));
+Blocks.navalFactory.plans.add(new UnitFactory.UnitPlan(SuNavT1, 60 * 25, ItemStack.with(Items.silicon, 45, Items.lead, 25, Items.titanium, 20)));
