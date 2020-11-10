b05307086a4c66a84752d58384054bc4a7923ece
MEEP of Faith <meepmatthew@gmail.com>
2020-11-10 11:32:14 -0800

    Hey, you, get in the water

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
-require("units/dart")
-require("units/arrow")
-require("units/spear")
-require("units/javelin")
-require("units/rana")
+require("units/pAir/dart")
+require("units/pAir/arrow")
+require("units/pAir/spear")
+require("units/pAir/javelin")
+
+require("units/sNaval/rana")
--- scripts/units/rana.js
+++
@@ -1,7 +0,0 @@
-const SNavT1 = extendContent(WaterMoveUnitType, "rana", {});
-SNavT1.constructor = () => extend(Unit, {});
-SNavT1.defaultController = () => new GroundAI();
-
-public static @EntityDef({Unitc.class, WaterMovec.class}) UnitType SNavT1;
-
-Blocks.navalFactory.plans.add(new UnitFactory.UnitPlan(SNavT1, 60 * 25, ItemStack.with(Items.silicon, 45, Items.lead, 25, Items.titanium, 20)));
