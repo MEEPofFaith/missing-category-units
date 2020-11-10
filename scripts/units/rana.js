a80195c34e0d80b254518421c1819c51c17226ea
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2020-11-10 14:09:12 -0500

    Update rana.js

--- scripts/units/rana.js
+++ scripts/units/rana.js
@@ -2,4 +2,5 @@ const SNavT1 = extendContent(UnitType, "rana", {});
 SNavT1.constructor = () => extend(BuilderMinerUnit, {});
 SNavT1.defaultController = () => new FlyingAI();
 
-RepairFieldAbility
+SpNavT1.abilities.add(new RepairFieldAbility(10, 60 * 5, 10));
+SpNavT1.abilities.add(new ShieldRegenFieldAbility(10, 50, 60 * 7, 10));
