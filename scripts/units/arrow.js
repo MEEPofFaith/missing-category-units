2e8ed0dda70bb8181498b8348866d6e9b7ba18c6
MEEP of Faith <meepmatthew@gmail.com>
2020-11-09 21:16:34 -0800

    Haha lasers go brrrr

--- scripts/units/arrow.js
+++ scripts/units/arrow.js
@@ -2,3 +2,4 @@ const SpAirT2 = extendContent(UnitType, "arrow", {});
 SpAirT2.constructor = () => extend(BuilderMinerUnit, {});
 SpAirT2.defaultController = () => new FlyingAI();
 SpAirT2.abilities.add(new MoveLightningAbility(27, 12, 0.15, 1.1, 4.5, Color.valueOf("#bf92f9")));
+SpAirT2.ammoType = AmmoTypes.powerLow;
--- scripts/units/spear.js
+++ scripts/units/spear.js
@@ -3,3 +3,4 @@ SpAirT3.constructor = () => extend(BuilderMinerUnit, {});
 SpAirT3.defaultController = () => new FlyingAI();
 SpAirT3.abilities.add(new MoveLightningAbility(35, 16, 0.2, 1.2, 5, Color.valueOf("#bf92f9")));
 SpAirT3.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 300, 390, 48));
+SpAirT3.ammoType = AmmoTypes.power;
