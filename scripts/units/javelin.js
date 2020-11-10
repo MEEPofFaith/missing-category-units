a94b88f1b105356afa3836453a9e7e79a0ddeaa5
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2020-11-10 08:34:08 -0500

    please no
    just no

--- scripts/units/javelin.js
+++ scripts/units/javelin.js
@@ -1,7 +1,7 @@
 const SpAirT4 = extendContent(UnitType, "javelin", {});
 SpAirT4.constructor = () => extend(BuilderMinerUnit, {});
 SpAirT4.defaultController = () => new FlyingAI();
-SpAirT4.abilities.add(new MoveLightningAbility(58, 20, 0.25, 1.3, 5.5, Color.valueOf("#bf92f9")));
+//SpAirT4.abilities.add(new MoveLightningAbility(58, 20, 0.25, 1.3, 5.5, Color.valueOf("#bf92f9"))); just no
 SpAirT4.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 450, 510, 64));
 SpAirT4.ammoType = AmmoTypes.powerHigh;
 
