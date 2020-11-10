2cf32b9003a4d0484606f28348c1a0cde91278f3
MEEP of Faith <meepmatthew@gmail.com>
2020-11-10 15:46:23 -0800

    These should be interceptors

--- scripts/units/pAir/javelin.js
+++ scripts/units/pAir/javelin.js
@@ -1,7 +1,27 @@
+const javelinController = prov(() => {
+  var jAI = extend(FlyingAI, {
+    updateMovement(){
+      if(this.target != null && this.command() == UnitCommand.attack){
+        this.attack(120);
+      }
+
+      if(this.target == null && this.command() == UnitCommand.attack && Vars.state.rules.waves && this.unit.team == Vars.state.rules.defaultTeam){
+        this.moveTo(this.getClosestSpawner(), Vars.state.rules.dropZoneRadius + 120);
+      }
+
+      if(this.command() == UnitCommand.rally){
+        this.moveTo(this.targetFlag(this.unit.x, this.unit.y, BlockFlag.rally, false), 60);
+      }
+    }
+  });
+  return jAI;
+});//Custom AI needed because no weapons
+
 const SpAirT4 = extendContent(UnitType, "javelin", {});
 SpAirT4.constructor = () => extend(BuilderMinerUnit, {});
-SpAirT4.defaultController = () => new FlyingAI();
-//SpAirT4.abilities.add(new MoveLightningAbility(58, 20, 0.25, 1.3, 5.5, Color.valueOf("#bf92f9"))); just no
+SpAirT4.defaultController = javelinController;
+SpAirT4.abilities.add(new MoveLightningAbility(58, 20, 0.25, 1.3, 5.5, Color.valueOf("#bf92f9")));
+//MEEP yes
 SpAirT4.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 8, 60 * 8.75, 64));
 SpAirT4.ammoType = AmmoTypes.powerHigh;
 
--- scripts/units/pAir/spear.js
+++ scripts/units/pAir/spear.js
@@ -1,6 +1,25 @@
+const spearController = prov(() => {
+  var sAI = extend(FlyingAI, {
+    updateMovement(){
+      if(this.target != null && this.command() == UnitCommand.attack){
+        this.attack(120);
+      }
+
+      if(this.target == null && this.command() == UnitCommand.attack && Vars.state.rules.waves && this.unit.team == Vars.state.rules.defaultTeam){
+        this.moveTo(this.getClosestSpawner(), Vars.state.rules.dropZoneRadius + 120);
+      }
+
+      if(this.command() == UnitCommand.rally){
+        this.moveTo(this.targetFlag(this.unit.x, this.unit.y, BlockFlag.rally, false), 60);
+      }
+    }
+  });
+  return sAI;
+});//Custom AI needed because no weapons
+
 const SpAirT3 = extendContent(UnitType, "spear", {});
 SpAirT3.constructor = () => extend(BuilderMinerUnit, {});
-SpAirT3.defaultController = () => new FlyingAI();
+SpAirT3.defaultController = spearController;
 SpAirT3.abilities.add(new MoveLightningAbility(35, 16, 0.2, 1.2, 5, Color.valueOf("#bf92f9")));
 SpAirT3.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 5, 60 * 6, 48));
 SpAirT3.ammoType = AmmoTypes.power;
