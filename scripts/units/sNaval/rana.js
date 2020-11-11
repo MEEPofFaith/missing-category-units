cff8f37779cd3fcf0d91851e4772df5c9e56669d
MEEP of Faith <meepmatthew@gmail.com>
2020-11-10 21:59:12 -0800

    oop

--- scripts/units/sNaval/rana.js
+++ scripts/units/sNaval/rana.js
@@ -58,11 +58,11 @@ const ranaController = prov(() => {
 const SuNavT1 = extendContent(UnitType, "rana", {});
 SuNavT1.constructor = () => extend(UnitWaterMove, {});
 //SuNavT1.defaultController = ranaController;
-SuNavT1.defaultController = () => new groundAI();
+SuNavT1.defaultController = () => new GroundAI();
 
 SuNavT1.abilities.add(new RepairFieldAbility(10, 60 * 5, 24));
 SuNavT1.abilities.add(new ShieldRegenFieldAbility(40, 50, 60 * 8, 24));
 
-SuNavT1.ammoType = AmmoTypes.powerHigh;
+SuNavT1.ammoType = AmmoTypes.power;
 
 Blocks.navalFactory.plans.add(new UnitFactory.UnitPlan(SuNavT1, 60 * 25, ItemStack.with(Items.silicon, 45, Items.metaglass, 25, Items.titanium, 20)));
