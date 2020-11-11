14177142386df6d61a3ccdc383750e91f781c9ee
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2020-11-11 16:15:05 -0500

    Update dart.js

--- scripts/units/pAir/dart.js
+++ scripts/units/pAir/dart.js
@@ -27,7 +27,11 @@ const dartController = prov(() => {
 });//Custom AI needed because no weapons
 
 const SpAirT1 = extendContent(UnitType, "dart", {});
-SpAirT1.constructor = () => extend(UnitBuilder, {});
+
+    //air + building + mining
+    //public static @EntityDef({Unitc.class, Builderc.class}) UnitType poly;
+
+SpAirT1.constructor = () => extend(BuilderUnit, {});
 SpAirT1.defaultController = dartController;
 SpAirT1.abilities.add(new MoveLightningAbility(15, 8, 0.15, 1, 4, Color.valueOf("#bf92f9")));
 
