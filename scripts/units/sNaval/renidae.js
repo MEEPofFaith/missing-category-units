5dd40be563ce0aa48822ff39f876e58e05ee3579
MEEP of Faith <meepmatthew@gmail.com>
2020-11-13 14:56:22 -0800

    Fix spawning

--- scripts/units/sNaval/renidae.js
+++ scripts/units/sNaval/renidae.js
@@ -4,7 +4,7 @@ SuNavT2.constructor = () => {
   return unit;
 }
 
-SuNavT2.abilities.add(new UnitSpawnAbility(UnitTypes.Mono, 60*20, 0, 0));
+SuNavT2.abilities.add(new UnitSpawnAbility(UnitTypes.mono, 60 * 30, 0, -5));
 SuNavT2.ammoType = AmmoTypes.powerLow;
 
 var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "purple-air-rana"), Vars.content.getByName(ContentType.unit, "purple-air-renidae")]);
