472609f35edae2469301863607654b0ceb2838b6
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2020-11-13 19:01:53 -0500

    Update renidae.js

--- scripts/units/sNaval/renidae.js
+++ scripts/units/sNaval/renidae.js
@@ -1,6 +1,7 @@
 const SuNavT2 = extendContent(UnitType, "renidae", {});
 SuNavT2.constructor = () => extend(UnitWaterMove, {});
 SuNavT2.abilities.add(new UnitSpawnAbility(UnitTypes.mono, 60 * 30, 0, -5));
+SpAirT4.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 8, 60 * 8.75, 64));
 SuNavT2.ammoType = AmmoTypes.powerLow;
 
 var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "purple-air-rana"), Vars.content.getByName(ContentType.unit, "purple-air-renidae")]);
