9c193c40e3f3e04d3c12e4f139e3fa508c8456b3
MEEP of Faith <meepmatthew@gmail.com>
2020-11-15 17:55:01 -0800

    Mod description color fixes.

---
+++ scripts/mod-meta.js
@@ -0,0 +1,4 @@
+const meta = Vars.mods.locateMod("purple-air").meta;
+meta.displayName = "[#84F491]Missing [#BF92F9]Category [#8AA3F4]Units"
+meta.author = "Scripting: [#FCC21B]MEEP of Faith[]\nSpriting: [#A200FF]A5TR0spud[]";
+meta.description = "Adds units for missing categories of units,\n[#BF92F9]Purple Air,[] [#84F491]Green Naval,[] and [#BF92F9]Purple Naval.[]\nThe fourth slot in the factories (will eventually) have [#8AA3F4]blue units.[]\n\n[#404040]Note: Android 7 and below will not be able to run this mod.";
