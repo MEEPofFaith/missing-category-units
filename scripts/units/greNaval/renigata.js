8afa02e7b2e16507c0f0a8ce2e0a913779b9d2e3
MEEP of Faith <meepmatthew@gmail.com>
2020-11-19 16:31:41 -0800

    Renigata spawn locations

--- scripts/units/greNaval/renigata.js
+++ scripts/units/greNaval/renigata.js
@@ -7,12 +7,12 @@ SuNavT4.defaultController = ais.groundRepairAI;
 var flareSpawnSeconds = 20;
 var polySpawnSeconds = 25;
 
-SuNavT4.abilities.add(new UnitSpawnAbility(UnitTypes.poly, polySpawnSeconds * 60, 13.5, -8.5));
-SuNavT4.abilities.add(new UnitSpawnAbility(UnitTypes.poly, polySpawnSeconds * 60, -13.5, -8.5));
-SuNavT4.abilities.add(new UnitSpawnAbility(UnitTypes.flare, flareSpawnSeconds * 60, 5.75, -2));
-SuNavT4.abilities.add(new UnitSpawnAbility(UnitTypes.flare, flareSpawnSeconds * 60, -5.75, -2));
+SuNavT4.abilities.add(new UnitSpawnAbility(UnitTypes.poly, polySpawnSeconds * 60, 34/4, -68/4));
+SuNavT4.abilities.add(new UnitSpawnAbility(UnitTypes.poly, polySpawnSeconds * 60, -34/4, -68/4));
+SuNavT4.abilities.add(new UnitSpawnAbility(UnitTypes.flare, flareSpawnSeconds * 60, 32/4, -6/4));
+SuNavT4.abilities.add(new UnitSpawnAbility(UnitTypes.flare, flareSpawnSeconds * 60, -32/4, -6/4));
 SuNavT4.abilities.add(new ShieldRegenFieldAbility(40, 120, 60 * 16, 32));
-SuNavT4.ammoType = AmmoTypes.powerLow;
+SuNavT4.ammoType = AmmoTypes.power;
 
 var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "purple-air-protidae"), Vars.content.getByName(ContentType.unit, "purple-air-renigata")]);
 Blocks.exponentialReconstructor.upgrades.add(upgrade.toArray(UnitType));
