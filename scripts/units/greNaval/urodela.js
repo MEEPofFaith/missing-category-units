a4e6d85acf33418db9bbd29eed5bb605434e031a
MEEP of Faith <meepmatthew@gmail.com>
2020-12-11 10:59:16 -0800

    Healxopid

--- scripts/units/greNaval/urodela.js
+++ scripts/units/greNaval/urodela.js
@@ -49,7 +49,104 @@ ffTBDouble.bullet = tractorBeam;
 ffTBDouble.recoil = 0;
 ffTBDouble.alternate = false;
 
+//Fx.sapExplosion, but green
+const plasmaExplosion = new Effect(25, e => {
+  Draw.color(Pal.heal);
+
+  e.scaled(6, s => {
+    Lines.stroke(3 * s.fout());
+    Lines.circle(e.x, e.y, 3 + s.fin() * 80);
+  });
+
+  Draw.color(Color.white);
+
+  Angles.randLenVectors(e.id, 9, 2 + 70 * e.finpow(), (x, y) => {
+    Fill.circle(e.x + x, e.y + y, e.fout() * 4 + 0.5);
+  });
+
+  Draw.color(Color.valueOf("73D188"));
+  Lines.stroke(1 * e.fout());
+
+  Angles.randLenVectors(e.id + 1, 8, 1 + 60 * e.finpow(), (x, y) => {
+    Lines.lineAngle(e.x + x, e.y + y, Mathf.angle(x, y), 1 + e.fout() * 3);
+  });
+})
+
+const cluster = extend(BasicBulletType, {});
+cluster.speed = 3;
+cluster.sprite = "large-bomb";
+cluster.hitEffect = plasmaExplosion;
+cluster.knockback = 0.6;
+cluster.lifetime = 80;
+cluster.width = 20;
+cluster.height = 20;
+cluster.shrinkX = 0.5;
+cluster.shrinkY = 0.5;
+cluster.damage = 10;
+cluster.splashDamageRadius = 50;
+cluster.splashDamage = 15;
+cluster.backColor = Pal.heal;
+cluster.frontColor = Color.white;
+cluster.mixColorTo = Color.white;
+cluster.hitSound = Sounds.plasmaboom;
+cluster.lightningColor = Pal.heal;
+cluster.lightning = 2;
+cluster.lightningLength = 5;
+cluster.smokeEffect = Fx.shootBigSmoke2;
+cluster.hitShake = 3;
+cluster.scaleVelocity = true;
+cluster.collidesTiles = false;
+cluster.collides = false;
+cluster.collidesAir = false;
+cluster.shootEffect = Fx.shootBig;
+cluster.trailEffect = Fx.artilleryTrail;
+cluster.healPercent = 10;
+
+const shell = extend(BasicBulletType, {});
+shell.speed = 3;
+shell.damage = 16;
+shell.splashDamage = 40;
+shell.splashDamageRadius = 60;
+shell.knockback = 0.5;
+shell.sprite = "large-bomb";
+shell.width = 30;
+shell.height = 30;
+shell.shrinkX = 0.75;
+shell.shrinkY = 0.75;
+shell.spin = 2;
+shell.lifetime = 60;
+shell.despawnEffect = Fx.greenBomb;
+shell.hitEffect = Fx.massiveExplosion;
+shell.backColor = Pal.heal;
+shell.frontColor = Color.white;
+shell.mixColorTo = Color.white;
+shell.hitSound = Sounds.plasmaboom;
+shell.scaleVelocity = true;
+shell.collidesTiles = false;
+shell.collides = false;
+shell.collidesAir = false;
+shell.scaleVelocity = true;
+shell.hitShake = 1;
+shell.shootEffect = Fx.shootBig;
+shell.trailEffect = Fx.artilleryTrail;
+shell.healPercent = 10;
+shell.fragLifeMin = 0.3;
+//shell.fragLifeMax = 2;
+shell.fragBullets = 20;
+shell.fragBullet = cluster;
+
 //Back center: (0, -116) TODO: Giant heal artillery (Base off Toxopid)
+const healxopid = extendContent(Weapon, "purple-air-urodela-cannon", {});
+healxopid.x = 0;
+healxopid.y = -116 / 4;
+healxopid.shootY = 22;
+healxopid.mirror = false;
+healxopid.reload = 140
+healxopid.rotateSpeed = 1.5;
+healxopid.ejectEffect = Fx.casing3;
+healxopid.rotate = true;
+healxopid.occlusion = 30;
+healxopid.bullet = shell;
 //Front center: (0, 56) TODO: ???
 
 var monoSec = 60;
@@ -74,6 +171,7 @@ SuNavT5.ammoType = AmmoTypes.powerHigh;
 SuNavT5.weapons.add(ffTBDouble);
 SuNavT5.weapons.add(fTBDouble);
 SuNavT5.weapons.add(tBDouble);
+SuNavT5.weapons.add(healxopid);
 
 var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "purple-air-renigata"), Vars.content.getByName(ContentType.unit, "purple-air-urodela")]);
 Blocks.tetrativeReconstructor.upgrades.add(upgrade.toArray(UnitType));
