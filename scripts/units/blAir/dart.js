2fa59934c34677482baf59324c5e069a80e769eb
MEEP of Faith <meepmatthew@gmail.com>
2020-12-13 18:43:19 -0800

    Please do not the schwing schwing

--- scripts/units/blAir/dart.js
+++ scripts/units/blAir/dart.js
@@ -2,7 +2,7 @@ const ais = this.global.mcu.ai;
 
 const SpAirT2 = extendContent(UnitType, "dart", {});
 SpAirT2.constructor = () => extend(UnitEntity, {});
-SpAirT2.defaultController = ais.flareAI(75, [BlockFlag.factory, BlockFlag.battery, BlockFlag.generator, BlockFlag.core]);
+SpAirT2.defaultController = ais.flareAI(95, [BlockFlag.factory, BlockFlag.battery, BlockFlag.generator, BlockFlag.core]);
 SpAirT2.abilities.add(new MoveLightningAbility(16, 11, 0.1, 1.1, 3.3, Color.valueOf("#a9d8ff")));
 SpAirT2.ammoType = AmmoTypes.powerLow;
 
--- scripts/units/blAir/javelin.js
+++ scripts/units/blAir/javelin.js
@@ -2,7 +2,7 @@ const ais = this.global.mcu.ai;
 
 const SpAirT4 = extendContent(UnitType, "javelin", {});
 SpAirT4.constructor = () => extend(UnitEntity, {});
-SpAirT4.defaultController = ais.flareAI(112, [BlockFlag.turret, BlockFlag.factory, BlockFlag.battery, BlockFlag.generator, BlockFlag.core]);
+SpAirT4.defaultController = ais.flareAI(160, [BlockFlag.turret, BlockFlag.factory, BlockFlag.battery, BlockFlag.generator, BlockFlag.core]);
 SpAirT4.abilities.add(new MoveLightningAbility(21, 17, 0.1, 1.3, 4.2, Color.valueOf("#a9d8ff")));
 SpAirT4.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 3, 60 * 4.25, 48));
 SpAirT4.ammoType = AmmoTypes.powerHigh;
