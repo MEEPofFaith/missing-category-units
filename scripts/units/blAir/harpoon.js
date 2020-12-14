32dbdb7e2c3388100fa4037c6e1870cbcdf44741
MEEP of Faith <meepmatthew@gmail.com>
2020-12-13 18:38:07 -0800

    I don't care if you miss, just shoot stuff already

--- scripts/units/blAir/harpoon.js
+++ scripts/units/blAir/harpoon.js
@@ -2,7 +2,7 @@ const ais = this.global.mcu.ai;
 
 const SpAirT5 = extendContent(UnitType, "harpoon", {});
 SpAirT5.constructor = () => extend(UnitEntity, {});
-SpAirT5.defaultController = ais.flareAI(136, [Blockflag.turret, Blockflag.factory, Blockflag.core]);
+SpAirT5.defaultController = ais.flareAI(136, [BlockFlag.turret, BlockFlag.factory, BlockFlag.core]);
 SpAirT5.abilities.add(new MoveLightningAbility(25, 20, 0.1, 1.4, 4.3, Color.valueOf("#a9d8ff")));
 SpAirT5.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 4, 60 * 5, 64));
 SpAirT5.ammoType = AmmoTypes.powerHigh;
